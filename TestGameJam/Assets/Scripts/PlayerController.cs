b4231ca5841227becd1053c209931160c377cb17
JakobiTalbot <jakobi.talbot@gmail.com>
2019-04-11 13:12:15 +1000

    - Added punching

--- TestGameJam/Assets/Scripts/PlayerController.cs
+++ TestGameJam/Assets/Scripts/PlayerController.cs
@@ -10,13 +10,27 @@ public class PlayerController : MonoBehaviour
     public float m_rotateSpeed = 0.5f;
     public float m_jumpVelocity = 20f;
     public float m_thumbstickDeadzone = 0.15f;
+    public float m_punchCooldownTime = 0.2f;
+    public float m_punchDistance = 1f;
+    public float m_punchSpeed = 0.5f;
+    public float m_slowdownWhenPunch = 0.2f;
+    public GameObject[] m_fists;
 
     private Rigidbody m_rigidbody;
+    private Vector3[] m_fistStartPos;
+    private float m_fCurrentPunchCooldown = 0f;
+    private bool m_bUseRightFist = true;
+    private bool m_bPunching = false;
 
 	// Use this for initialization
 	void Start()
     {
         m_rigidbody = GetComponent<Rigidbody>();
+        m_fistStartPos = new Vector3[2];
+        for (int i = 0; i < 2; i++)
+        {
+            m_fistStartPos[i] = m_fists[i].transform.localPosition;
+        }
 	}
 	
 	// Update is called once per frame
@@ -33,13 +47,77 @@ public class PlayerController : MonoBehaviour
             v3Velocity.z += gamePadState.ThumbSticks.Left.Y * m_movementSpeed * Time.deltaTime;
         }
         // jump
-        if (gamePadState.Buttons.A == ButtonState.Pressed && Physics.Raycast(transform.position, Vector3.down, 0.5f))
-            v3Velocity.y = m_jumpVelocity;
+        if (gamePadState.Buttons.A == ButtonState.Pressed
+            || gamePadState.Triggers.Left > 0f)
+            if (Physics.Raycast(transform.position, Vector3.down, 1f))
+                v3Velocity.y = m_jumpVelocity;
         // set new velocity
         m_rigidbody.velocity = v3Velocity;
 
         // look towards right stick input
         if (Mathf.Abs(gamePadState.ThumbSticks.Right.X) > m_thumbstickDeadzone || Mathf.Abs(gamePadState.ThumbSticks.Right.Y) > m_thumbstickDeadzone)
             transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(new Vector3(gamePadState.ThumbSticks.Right.X, 0, gamePadState.ThumbSticks.Right.Y)), m_rotateSpeed);
+
+        // start punch
+        if (gamePadState.Triggers.Right > 0f
+            && m_fCurrentPunchCooldown <= 0f
+            && !m_bPunching)
+        {
+            m_bPunching = true;
+            m_rigidbody.velocity *= m_slowdownWhenPunch;
+        }
+
+        // stop punch if function returns false
+        if (m_bPunching
+            && !Punch(m_bUseRightFist))
+        {
+            m_bPunching = false; // stop punching
+            m_fCurrentPunchCooldown = m_punchCooldownTime; // set cooldown
+            m_bUseRightFist = !m_bUseRightFist; // alternate fists
+        }
+
+        // decrement cooldown of punch
+        if (!m_bPunching
+            && m_fCurrentPunchCooldown > 0f)
+        {
+            m_fCurrentPunchCooldown -= Time.deltaTime;
+        }
+    }
+
+    // returns false when punch has ended
+    // otherwise returns true
+    private bool Punch(bool bUseRightFist)
+    {
+        // punch with right fist
+        if (bUseRightFist)
+        {
+            // move fist
+            Vector3 v3FistPos = m_fists[1].transform.localPosition;
+            Vector3 v3PunchEndPos = m_fistStartPos[1] + new Vector3(0, 0, m_punchDistance);
+            v3FistPos = Vector3.Lerp(v3FistPos, v3PunchEndPos, m_punchSpeed);
+            m_fists[1].transform.localPosition = v3FistPos;
+            if (Vector3.Distance(v3FistPos, v3PunchEndPos) < 0.1f)
+            {
+                // reset position
+                m_fists[1].transform.localPosition = m_fistStartPos[1];
+                return false;
+            }
+        }
+        // punch with left fist
+        else
+        {
+            // move fist
+            Vector3 v3FistPos = m_fists[0].transform.localPosition;
+            Vector3 v3PunchEndPos = m_fistStartPos[0] + new Vector3(0, 0, m_punchDistance);
+            v3FistPos = Vector3.Lerp(v3FistPos, v3PunchEndPos, m_punchSpeed);
+            m_fists[0].transform.localPosition = v3FistPos;
+            if (Vector3.Distance(v3FistPos, v3PunchEndPos) < 0.1f)
+            {
+                // reset position
+                m_fists[0].transform.localPosition = m_fistStartPos[0];
+                return false;
+            }
+        }
+        return true;
     }
 }
